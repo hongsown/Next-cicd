ea4c227faa11e1d38bacef41e3e702e97c928b02
hongsown <sonnguyen352003@gmail.com>
2024-11-22 21:27:02 +0700

    first commit

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,17 @@
+type Params = {
+  params: {
+    slug: string;
+  };
+};
+
+export async function generateMetadata({ params }: Params) {
+  return { title: `Post: ${params.slug}` };
+}
+
+export default function Page({ params }: Params) {
+  return (
+    <h1>
+      Slug: {params.slug} <p>Sonnguyen</p>
+    </h1>
+  );
+}
